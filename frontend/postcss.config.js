782dca0865bb57654233a9d6aac3d03f00a3df95
meharfatima83 <meharfatima0522@gmail.com>
2025-06-21 19:46:54 +0530

    chnges done

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
     tailwindcss: {},
     autoprefixer: {},
   },
-}
-
-=======
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
->>>>>>> fce50fa8c989488e3695f25e29e926acf26d6f13
+};
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -5,12 +5,14 @@ import { FeaturedProducts } from '@/components/FeaturedProducts';
 import { Categories } from '@/components/Categories';
 import { Testimonials } from '@/components/Testimonials';
 import { Footer } from '@/components/Footer';
+// import BrowsePage from '@/components/products/BrowsePage'; // Uncomment if you want to use it
 
 const LandingPage = () => {
   return (
     <div>
       <HeroSection />
       <FeaturedProducts />
+      {/* <BrowsePage /> */}
       <Categories />
       <Testimonials />
       <Footer />
